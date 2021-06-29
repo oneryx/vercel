19db0af07f5e6990c60af713d120deb2f89ebeda
xiang yang <oneryx@gmail.com>
2021-06-29 19:43:03 +1200

    change package name

--- api/date.go
+++ api/date.go
@@ -1,4 +1,4 @@
-package handler
+package api
 
 import (
 	"fmt"
@@ -7,5 +7,5 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, time.Now().String())
+	fmt.Fprintf(w, time.Now().Format("2006-01-02 15:04:05"))
 }
